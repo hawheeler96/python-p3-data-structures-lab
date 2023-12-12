7475792eda5265bd328ab9c439e10da2895caa91
Holly Wheeler <hollyawheeler.96@gmail.com>
2023-12-11 16:03:01 -0800

    lab complete

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,6 @@
+from venv import create
+
+
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -16,23 +19,64 @@ spicy_foods = [
     },
 ]
 
+
 def get_names(spicy_foods):
-    pass
+    names = [food["name"] for food in spicy_foods]
+    return names
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods_list = [food for food in spicy_foods if food.get("heat_level") > 5]
+    return spiciest_foods_list
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food.get("name")
+        cuisine = food.get("cuisine")
+        heat_level = food.get("heat_level")
+
+        print_peppers = "🌶" * heat_level
+        print(f"{name} ({cuisine}) | Heat Level: {print_peppers}")
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        list_cuisine = food.get("cuisine")
+
+        if list_cuisine == cuisine:
+            return food
+
+
+get_spicy_food_by_cuisine(spicy_foods, "Thai")
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food.get("name")
+        cuisine = food.get("cuisine")
+        heat_level = food.get("heat_level")
+
+        print_peppers = "🌶" * heat_level
+        if heat_level > 5:
+            print(f"{name} ({cuisine}) | Heat Level: {print_peppers}")
+
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total_heat_level = 0
+    num_spicy_foods = len(spicy_foods)
+    
+    for food in spicy_foods:
+        total_heat_level += food.get("heat_level")
+        
+    avg_heat = total_heat_level / num_spicy_foods
+    return avg_heat
+        
+
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods_copy = spicy_foods.copy()
+    spicy_foods_copy.append(spicy_food)
+    return spicy_foods_copy
+
+create_spicy_food(spicy_foods, {"name": "Griot", "cuisine": "Haitian", "heat_level": 10})
